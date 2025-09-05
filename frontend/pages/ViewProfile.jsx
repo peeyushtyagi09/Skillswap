d9eb51d44c67583beebee2f97c4c97d690094e74
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2025-09-05 12:06:34 +0530

    fix the loder logic in ViewPrrofile.jsx

--- frontend/pages/ViewProfile.jsx
+++ frontend/pages/ViewProfile.jsx
@@ -66,8 +66,10 @@ export default function ViewProfile() {
           style={{ WebkitOverflowScrolling: 'touch' }}
         >
           {/* States */}
-          {loading && (
-           <Loader2 />
+          {loading && ( 
+            <div className="flex items-center justify-center h-full w-full">
+            <div className="w-10 h-10 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+          </div>
           )}
 
           {!loading && error && (
